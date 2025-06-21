beb9972a39255a6ee3d9534b0ef9e7618de834f2
Nazer Hussain A <nazerhussain1999@gmail.com>
2025-06-22 01:50:40 +0530

    reload

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/personal-portfolio/",
+  base: "/personal-portfolio/",    
   server: {
     host: "::",
     port: 8080,
