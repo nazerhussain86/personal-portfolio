b37f07517783e4388b1fa144fd636a0f9e500373
Nazer Hussain A <nazerhussain1999@gmail.com>
2025-06-22 00:49:14 +0530

    live

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 // App.tsx
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -18,7 +17,6 @@ const App = () => (
       <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </HashRouter>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { HashRouter } from 'react-router-dom';
+// main.tsx
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-        <HashRouter>
-                <App />
-        </HashRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
