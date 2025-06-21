0e3bd68320dee1f648f410e1b5822dcb15a54321
Nazer Hussain A <nazerhussain1999@gmail.com>
2025-06-22 02:06:45 +0530

    reload

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { HashRouter } from 'react-router-dom';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-        <HashRouter>
-                <App />
-        </HashRouter>
-);
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("Root container missing in index.html");
+}
+
+const root = createRoot(container);
+root.render(<App />);
